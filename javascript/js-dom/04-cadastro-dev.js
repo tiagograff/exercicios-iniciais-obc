53f1aed2121b817abbf658f881cc4894494cb0cb
tiagograff <tiagograff00@gmail.com>
2026-06-12 17:30:31 -0300

    feat: mostrar os dados

--- javascript/js-dom/04-cadastro-dev.js
+++ javascript/js-dom/04-cadastro-dev.js
@@ -1,5 +1,11 @@
 //botoes
 const formBtn = document.getElementById("createFormBtn");
+const renderBtn = document.getElementById("renderDevsBtn");
+//elementos
+const sectionDevs = document.querySelector(".devInformations");
+const renderArea = document.getElementById("renderArea");
+//listas
+const devsList = [];
 
 //funcionalidades
 const cleanForm = () => {
@@ -12,6 +18,37 @@ const cleanForm = () => {
     .forEach((input) => (input.checked = false));
 };
 
+const createRenderDevsBtn = () => {
+  const renderBtn = document.createElement("button");
+  renderBtn.id = "renderDevsBtn";
+  renderBtn.innerText = "Mostrar Devs Cadastrados";
+  renderBtn.style.maxWidth = "max-content";
+  sectionDevs.appendChild(renderBtn);
+
+  //evento para o botao
+renderBtn.addEventListener("click", (ev) => {
+  ev.preventDefault();
+  renderArea.replaceChildren();
+
+  const renderList = document.createElement("ul");
+
+  devsList.forEach((dev) => {
+    const devItem = document.createElement("li");
+    let skills = "";
+    dev.experiences.forEach((exp) => {
+      skills += `\n- ${exp.tech} (${exp.experience})`;
+    });
+
+        devItem.textContent =
+        `Nome: ${dev.name}${skills}`;
+
+        renderList.appendChild(devItem);
+    });
+
+    renderArea.appendChild(renderList);
+    });
+};
+
 //form
 formBtn.addEventListener("click", (ev) => {
   ev.preventDefault();
@@ -20,9 +57,11 @@ formBtn.addEventListener("click", (ev) => {
     alert("Digite o nome do dev para criar o form");
   } else {
     const newDev = {
-      name: devNameInput,
-      experience: [],
-    };
+        name: devNameInput,
+        experiences: []
+    }
+
+    devsList.push(newDev);
     const subtitle = document.querySelector(".subtitle");
     subtitle.innerText = `Informações de ${devNameInput}`;
 
@@ -41,8 +80,13 @@ formBtn.addEventListener("click", (ev) => {
         experience: techYearsXp.value,
       };
       if (form.checkVisibility()) {
-        newDev.experience.push(newExperience);
-        cleanForm()
+        newDev.experiences.push(newExperience);
+        cleanForm();
+      } else {
+        alert("Preencha o formuláro corretamente");
+      }
+      if (!sectionDevs.querySelector("#renderDevsBtn")) {
+        createRenderDevsBtn();
       }
     });
   }
